509a969ab25f2d5a08b258f68324dbd6823071c2
leticiasilvas <127620091+leticiasilvas@users.noreply.github.com>
2023-11-18 17:58:37 -0300

    Add files via upload

---
+++ script01.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// trabalhar com variáveis, console.log
+
+// utilizando padrão camelCase
+
+// variavel (var, let ou const) -> identificador -> dados 
+const carro = "carro"
+const fabricanteCarro = "Renault"
+const modeloCarro = "Sandero"
+const corCarro = "Vermelha"
+const anoCarro = 2010
+
+// primeira forma: Concatenação
+console.log("carros: " + carro + ", fabricante: " + fabricanteCarro + ", modelo do carro: " + modeloCarro + ", cor do carro: " + corCarro + ", ano: " + anoCarro)
+
+// primeira forma: Interpolação
+console.log(`meu carro: ${carro}, fabricante do carro: ${fabricanteCarro}, modelo do carro: ${modeloCarro}, cor do carro: ${corCarro}, ano do carro: ${anoCarro}`);
+
+console.log(carro);
+console.log(fabricanteCarro);
+console.log(modeloCarro)
+console.log(corCarro);
+console.log(2010);
+
+
+
+
+//1-entrada de dados
+// var nome = valor
+// let PI = 3,14
+// let raio = 16
+
+//2-processamento
+// pi x raio ao quadrado
+
+//3-saída de dados 
+//log(resultado)
+
+
+
+
+let PI = 3,14
+let raio = 16
+
+console.log("3,14"*16)
